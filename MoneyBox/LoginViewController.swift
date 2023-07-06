7aec3a1f823675772bcfd614bbc33023c8bdead2
JonnySimmons <simmonsjonny@gmail.com>
2023-07-06 11:33:35 +0100

    Test 1

--- MoneyBox/LoginViewController.swift
+++ MoneyBox/LoginViewController.swift
@@ -9,4 +9,5 @@ import UIKit
 
 class LoginViewController: UIViewController {
     // Design Login
+    //test
 }
---
+++ MoneyBox/README.swift
@@ -0,0 +1,10 @@
+//
+//  README.swift
+//  MoneyBox
+//
+//  Created by Jonathan Simmons on 06/07/2023.
+//
+
+//import Foundation
+
+// Infomation on Jonathan Simmons project
